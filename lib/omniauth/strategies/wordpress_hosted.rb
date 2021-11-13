005c917fbfaa4ca67668d6b3f7c895d2ad2078aa
Kazufumi Takashina <takashina@renosys.co.jp>
2021-11-13 15:07:01 +0900

    change get token for WP Oauth Server v4

--- lib/omniauth/strategies/wordpress_hosted.rb
+++ lib/omniauth/strategies/wordpress_hosted.rb
@@ -40,6 +40,14 @@ module OmniAuth
                        }
         ).parsed
       end
+
+    protected
+
+      def build_access_token
+        verifier = request.params["code"]
+        client.auth_code.get_token(verifier)
+        #client.auth_code.get_token(verifier, {:redirect_uri => callback_url}.merge(token_params.to_hash(:symbolize_keys => true)), deep_symbolize(options.auth_token_params))
+      end
     end
   end
 end
